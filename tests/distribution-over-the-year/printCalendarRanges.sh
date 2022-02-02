bfd57098d1e15c2ffaf6741a340ab196e44af124
Ingo Karkat <swdev@ingo-karkat.de>
2022-02-02 18:39:26 +0100

    ENH: Implement weekly calendar
    By duplicating the printSection() AWK and printDistribution() shell functions.
    The alignment of the month has to be tweaked a bit for the weekly calendar; do that via a conditional.

--- tests/distribution-over-the-year/printCalendarRanges.sh
+++ tests/distribution-over-the-year/printCalendarRanges.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-earliestMonthDates=("2023-"{01..12}"-01 12:00")
-earliestMonthDates=("${earliestMonthDates[@]}" "${earliestMonthDates[@]}" "${earliestMonthDates[@]}" "${earliestMonthDates[@]}")
-latestMonthDates=("2024-"{01..12}"-01 12:00")
+earliestMonthDates=("2024-"{01..12}"-01 12:00")
+latestMonthDates=("2023-"{01..12}"-01 12:00")
+latestMonthDates=("${latestMonthDates[@]}" "${latestMonthDates[@]}" "${latestMonthDates[@]}" "${latestMonthDates[@]}")
 earliestEndDates=("2024-12-31 12:00" "2024-12-31 12:00")
 latestEndDates=("2012-12-31 12:00" "2012-12-31 12:00" "2012-12-31 12:00" )
 
-echonl "${earliestMonthDates[@]}" "${earliestEndDates[@]}" "${latestMonthDates[@]}" "${latestEndDates[@]}" | \
-    distribution-over-the-year --graph-field 1 --as colorbox-calendar "$@"
+if contains --weekly "$@"; then
+    echonl "${earliestMonthDates[@]}" "${earliestEndDates[@]}" | \
+	distribution-over-the-year --graph-field 1 --as colorbox-calendar "$@"
+    echonl "${latestMonthDates[@]}" "${latestEndDates[@]}" | \
+	distribution-over-the-year --graph-field 1 --as colorbox-calendar "$@"
+else
+    echonl "${earliestMonthDates[@]}" "${earliestEndDates[@]}" "${latestMonthDates[@]}" "${latestEndDates[@]}" | \
+	distribution-over-the-year --graph-field 1 --as colorbox-calendar "$@"
+fi
